2c65188e68bed1a549ef8835e2061a47d3098787
Marcos Vinícius <marquinhosvinis@gmail.com>
2019-06-03 17:40:25 -0300

    Dimensões finais definidas, Plano de fundo atualizado

--- Physics on Python.py
+++ Physics on Python.py
@@ -12,23 +12,93 @@ from pymunk.vec2d import Vec2d
 #Math
 from math import sqrt, degrees
 import random
+
+class Player():
+    def __init__(self,mass,vertices,position):
+        self.mass = mass
+        self.vertices = vertices
+        self.shape = pymunk.Poly(None,self.vertices)
+        self.moment = pymunk.moment_for_poly(self.mass, self.shape.get_vertices())
+        self.body = pymunk.Body(self.mass,self.moment, pymunk.Body.DYNAMIC)
+        self.shape.body = self.body
+        self.body.position = position
+        self.shape.elasticity = 1
+        self.shape.friction = 1
+        self.existence = self.body, self.shape
+        self.vida = 1000
+        
+class Ball():
+    def __init__(self,radius,mass,position):
+        self.radius = radius
+        self.mass = mass
+        self.moment = pymunk.moment_for_circle(self.mass,0,self.radius,(0,0))
+        self.body = pymunk.Body(self.mass,self.moment, pymunk.Body.DYNAMIC)
+        self.body.position = position
+        self.shape = pymunk.Circle(self.body,self.radius,(0,0))
+        self.shape.elasticity = 1
+        self.body.velocity = 0,0
+        self.shape.friction = 1.0
+        self.combustivel = 3000
+        self.existence = self.body,self.shape
+
+class Segment():
+    def __init__(self,start_point,end_point,thickness): # start e end_point = (x,y)
+        self.start_point = start_point
+        self.end_point = end_point
+        self.thickness = thickness
+        self.space = pymunk.Space()
+        self.shape = pymunk.Segment(self.space.static_body,self.start_point,self.end_point,self.thickness)
+        self.shape.elasticity = 0.0001
+        self.shape.friction = 1
+
+class Dot():
+    def __init__(self, point):
+        self.point = point
+        self.thickness = 3
+        self.space = pymunk.Space()
+        self.shape = pymunk.Segment(self.space.static_body,self.point,self.point,self.thickness)
+        self.shape.elasticity = 1
+        self.shape.friction = 1
+
+class Box():
+    def __init__(self, mass, size, position): # size = (largura, altura); position = (x_centro, y_centro)
+        self.mass = mass
+        self.size = size
+        self.moment = pymunk.moment_for_box(self.mass,self.size)
+        self.body = pymunk.Body(self.mass,self.moment, pymunk.Body.DYNAMIC)
+        self.shape = pymunk.Poly.create_box(self.body, size)
+        self.body.position = position
+        self.shape.elasticity = 0.6
+        self.shape.friction = 1
+        self.existence = self.shape,self.body
+
+class Poly():
+    def __init__(self,mass,vertices,position):
+        self.mass = mass
+        self.vertices = vertices
+        self.shape = pymunk.Poly(None,self.vertices)
+        self.moment = pymunk.moment_for_poly(self.mass, self.shape.get_vertices())
+        self.body = pymunk.Body(self.mass,self.moment, pymunk.Body.DYNAMIC)
+        self.shape.body = self.body
+        self.body.position = position
+        self.shape.elasticity = 1
+        self.shape.friction = 1
+        self.existence = self.body, self.shape
+        self.vida = 1000
+
+
 # --------------------------------------------CLASSE PRINCIPAL---------------------------------------------------
 class Game:
     def __init__(self):
-        #Creating a window
-        self.width = 720
-        self.height = 480
+        self.width = 1350
+        self.height = 595
         self.FPS = 60
         self.window = pyglet.window.Window(self.width,self.height,"Physics On Python", resizable = False)
-        #Adding a icon
         icon = pyglet.image.load('icon.png')
         self.window.set_icon(icon)
-        
         #Music
         #music = pyglet.media.load('winter.wav', streaming = False)
         #music.play()
-
-
         self.screen = IntroScreen(self)
         self.register_event_handlers()
 
@@ -37,9 +107,16 @@ class Game:
         self.window.on_key_press = self.screen.on_key_press
         self.window.on_draw = self.screen.on_draw
 
-    def change_screen(self):
-        self.screen = AnotherScreen(self)
-        self.register_event_handlers()
+    def change_screen(self,symbol):
+        if symbol == key._1:
+            self.screen = AnotherScreen(self)
+            self.register_event_handlers()
+        if symbol == key._2:
+            self.screen = CarScreen(self)
+            self.register_event_handlers()
+        if symbol == key.ESCAPE:
+            self.screen = IntroScreen(self)
+            self.register_event_handlers()            
 
     def run(self):
         pyglet.app.run()
@@ -129,7 +206,6 @@ class Poly():
         self.vida = 1000
 
 
-# -----------------------------------------------------SCREENS---------------------------------------------------
 class Screen:
     def __init__(self, game):
         self.game = game
@@ -144,8 +220,6 @@ class Screen:
     def on_draw(self):
         pass
 
-
-
 class IntroScreen(Screen):
     def __init__(self, game):
         super(IntroScreen, self).__init__(game)
@@ -159,7 +233,7 @@ class IntroScreen(Screen):
         self.animSprite = pyglet.sprite.Sprite(self.animation,x=0, y=game.height-300)
 
         #Labels (texts on screen)
-        self.label = pyglet.text.Label("Physics On Python",
+        self.label = pyglet.text.Label("Simulador Físico Lunar",
                                     font_name = "Cambria Math",
                                     font_size = 40,
                                     color = (0, 0, 0, 255),
@@ -167,7 +241,7 @@ class IntroScreen(Screen):
                                     y = self.window.height / 2,
                                     anchor_x = "center",
                                     anchor_y = "center" )
-        self.welcome = pyglet.text.Label("Aperte ENTER para continuar",
+        self.welcome = pyglet.text.Label("Aperte 1 para Aeronave, Aperte 2 para carro",
                                     font_name = "Cambria Math",
                                     font_size = 20,
                                     color = (0, 0, 0, 255),
@@ -183,11 +257,9 @@ class IntroScreen(Screen):
             print("Right Mouse was pressed")
 
     def on_key_press(self, symbol, modifiers):
-        if symbol == key.A:
-            print("Olá")
-        if symbol == key.ENTER:
+        if symbol == key._1 or symbol == key._2:
             print("Mudando de tela")
-            self.game.change_screen()
+            self.game.change_screen(symbol)
 
     def on_draw(self):
         self.window.clear() # cleaning the window
@@ -214,8 +286,6 @@ class AnotherScreen(Screen):
         self.player.body.center_of_gravity = ( (0 + 171/5 + 171/10)/3, 300/15)
         self.player.body.elasticity = 0.0001
         self.player.body.friction = 1
-        
-        
         self.player_image = pyglet.image.load("nave.png")
        # self.player_image.anchor_x, self.player_image.anchor_y = self.player_image.width//2, self.player_image.height//2
         self.player_sprite = pyglet.sprite.Sprite(self.player_image, x = self.player.body.position[0], y = self.player.body.position[1])
@@ -226,32 +296,6 @@ class AnotherScreen(Screen):
         self.bussola.anchor_x, self.bussola.anchor_y = self.bussola.width//2, self.bussola.height//2
         self.bussola_sprite = pyglet.sprite.Sprite(self.bussola, x = game.width-50, y = game.height-50)
         
-        
-        
-#        self.meteoro_image = pyglet.image.load("rocha.png")
-#        self.meteoro_image.anchor_x, self.meteoro_image.anchor_y = self.meteoro_image.width//2, self.meteoro_image.height//2
-#        
-#        N = 10
-#        self.meteoros = {}
-#        for i in range(N):
-#            self.meteoros[i] = {
-#                    "meteoro": Box(50,(25,25),(random.randint(0,game.width),random.randint(0,game.height))),
-#                    "meteoro_image": pyglet.image.load("rocha.png"),
-#                    "meteoro_sprite": pyglet.sprite.Sprite(self.meteoro_image, x = self.meteoros[i]["meteoro"].body.position[0], y = self.meteoros[i]["meteoro"].body.position[1]),
-#                    "rotation": self.meteoros[i]["meteoro"].body.angle,
-#                    "existence": self.meteoros[i]["meteoro"].existence
-#                    }
-#        for meteoro in self.meteoros:
-#            self.space.add(self.meteoros[meteoro]["existence"])
-            
-            
-            # ELEMENTOS DINÂMICOS:
-        #self.player2 = Ball(30,20,(35,game.height-30))
-        #self.player2.body.velocity = (random.randint(-100,100),random.randint(-100,100))
-        #self.space.add(self.player2.existence)
-        #self.c = pymunk.PivotJoint(self.player.body, self.player2.body, (0,0))
-#        self.space.add(self.c)
-
             # ELEMENTOS ESTÁTICOS:
         self.segment1 = Segment((0,13),(game.width,13),2)
         self.segment2 = Segment((game.width,0),(game.width,game.height),2)
@@ -315,6 +359,9 @@ class AnotherScreen(Screen):
             if symbol == key.DOWN:
                 self.player.body.apply_impulse_at_local_point((0,-1000),self.player.body.center_of_gravity)
                 self.player.combustivel -= 10
+            if symbol == key.ESCAPE:
+                game.change_screen(symbol)
+
                
             # GRAVIDADE ZERO
             if symbol == key.SPACE:
@@ -387,12 +434,84 @@ class AnotherScreen(Screen):
         print("separate")
         return True
 
+class CarScreen(Screen):
+    def __init__(self, game):
+        super(CarScreen, self).__init__(game)
+
+        pyglet.clock.schedule_interval(self.update,1/60)
+        #Pymunk specifications
+        self.options = DrawOptions()
+        self.options.collision_point_color = (0,0,0,255)
+        self.space = pymunk.Space()
+        self.space.gravity = 0, 0
+        self.space.idle_speed_threshold = 10
+        self.space.sleep_time_threshold = 50
+        # Pymunk Space
+        self.distance_car_x = 300
+        self.distance_car_y = 300
+        self.car = Poly(50,( (0,0),(80,0),(80,30),(30,60),(0,60) ),(self.distance_car_x, self.distance_car_y))   
+        self.car.body.center_of_gravity = (40,30)
+        self.car.body.elasticity = 0.1
+        self.car.body.friction = 1
+        self.space.add(self.car.existence)
+        
+        self.roda_dianteira = Ball(20,20,(self.distance_car_x + 40, self.distance_car_y-30))
+        self.roda_dianteira.shape.elasticity = 0.1
+        self.roda_traseira = Ball(20,20,(self.distance_car_x- 50, self.distance_car_y-30))
+        self.roda_traseira.shape.elasticity = 0.1
+        self.c = pymunk.PinJoint(self.car.body, self.roda_traseira.body, (0, 0), (0, 0))
+        self.e = pymunk.SlideJoint(self.car.body, self.roda_traseira.body, (0, 60), (0, 0), 81,  100)
+        self.g = pymunk.DampedSpring(self.car.body, self.roda_traseira.body, (0, 60), (0, 0), 100, 1000,70)
+        self.d = pymunk.PinJoint(self.car.body, self.roda_dianteira.body,(80,0), (0,0))
+        self.f = pymunk.SlideJoint(self.car.body, self.roda_dianteira.body, (80,30), (0, 0), 60, 70)
+        self.h = pymunk.DampedSpring(self.car.body, self.roda_dianteira.body, (80,30), (0,0), 100, 1000,65)
+        self.space.add(self.c, self.d, self.roda_dianteira.existence, self.roda_traseira.existence, self.e, self.f, self.g, self.h)
+  
+        
+            # ELEMENTOS ESTÁTICOS:
+        self.segment1 = Segment((0,0),(game.width,0),2)
+        self.segment2 = Segment((game.width,0),(game.width,game.height),2)
+        self.segment3 = Segment((game.width,game.height),(0,game.height),2)
+        self.segment4 = Segment((0,game.height),(0,0),2)
+        self.space.add(self.segment1.shape, self.segment2.shape, self.segment3.shape, self.segment4.shape)
+
+    def on_mouse_press(self, x, y, button, modifier):
+        pass
 
 
-
+    def on_key_press(self, symbol, modifiers):
+        # AUMENTA A VELOCIDADE NOS RESPECTIVOS SENTIDOS
+        if symbol == key.RIGHT:
+            self.car.body.apply_impulse_at_local_point((10000,0),self.car.body.center_of_gravity)
+        if symbol == key.LEFT:
+            self.car.body.apply_impulse_at_local_point((-10000,0),self.car.body.center_of_gravity)
+#        if symbol == key.UP:
+#            self.player.body.velocity += (0,100)
+#        if symbol == key.DOWN:
+#            self.player.body.velocity -= (0,100)
+        # GRAVIDADE ZERO
+        if symbol == key.SPACE:
+            if self.space.gravity == (0, -300):
+                self.space.gravity = 0,0
+            else:
+                self.space.gravity = 0,-300
+        # PARA O TEMPO (USO NÃO RECOMENDADO CASO HAJA MUITOS ELEMENTOS NO ESPAÇO)
+        if symbol == key.T:
+            for bodies in self.space.bodies:
+                if bodies.body_type == pymunk.Body.DYNAMIC:
+                    if not bodies.is_sleeping:
+                        bodies.sleep()
+                    else:
+                        bodies.activate()
+        if symbol == key.ESCAPE:
+            game.change_screen(symbol)
+    def update(self,dt): #dt é "data time
+        self.space.step(dt)
+        
+    def on_draw(self):
+        self.window.clear()
+        self.space.debug_draw(self.options)
 #-----------------------------------------------------start------------------------------------------------------------
-
-
 if __name__ == '__main__': 
     game = Game()
     game.run()
